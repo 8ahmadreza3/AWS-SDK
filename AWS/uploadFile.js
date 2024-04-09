3049dca7d1d44bbe82ab8ed760eb25a1546b2847
8ahmadreza3 <8arhp3@gmail.com>
2024-04-09 19:02:14 +0330

    upload

---
+++ AWS/uploadFile.js
@@ -0,0 +1,15 @@
+const { PutObjectCommand } = require('@aws-sdk/client-s3')
+const client = require('./S3')
+const { v4: uuidv4 } = require('uuid')
+const getUrl = require('./getUrlFile')
+
+module.exports = async (fileContent) => {
+  const awsKey = uuidv4()
+  const params = {
+    Body: fileContent,
+    Bucket: process.env.BUCKET_NAME,
+    Key: `${awsKey}.png`
+  }
+  const response= await client.send(new PutObjectCommand(params))
+  return response
+}
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 require('dotenv').config()
+const fs = require('fs')
+const aws = require('./AWS')
+
+const file = fs.readFileSync('./img/3419668782265937.png')
+
+
+// const upload = async (file)=>{
+//   const res = await aws.upload(file)
+// }
+// upload(file)
+
+// const geturl = async (awsKey)=>{
+//   const res = await aws.getUrl(awskey)
+// }
+// geturl(awsKey)
+
+// const remove = async (awsKey)=>{
+//   const res = await aws.remove(awsKey)
+// }
+// remove(awsKey)
