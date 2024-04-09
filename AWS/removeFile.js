66e9d9bd6d488dede99862f9f078ac1678d261c9
8ahmadreza3 <8arhp3@gmail.com>
2024-04-09 19:02:06 +0330

    remove

---
+++ AWS/removeFile.js
@@ -0,0 +1,11 @@
+const { DeleteObjectCommand } = require('@aws-sdk/client-s3')
+const client = require('./S3.js')
+
+module.exports = async (awsKey) => {
+  const params = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: `${awsKey}.png`
+  }
+  const response = await client.send(new DeleteObjectCommand(params))
+  return response
+}
