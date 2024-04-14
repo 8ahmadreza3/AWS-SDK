3c52e8b221944c227023247ae9969a6a29eb35f1
8ahmadreza3 <8arhp3@gmail.com>
2024-04-14 20:06:20 +0330

    package update

--- AWS/index.js
+++ AWS/index.js
@@ -1,9 +1,11 @@
 const upload = require('./uploadFile')
 const getUrl = require('./getUrlFile')
 const remove = require('./removeFile')
+const filesList = require('./filesList')
 
 module.exports = {
   upload,
   getUrl,
-  remove
+  remove,
+  filesList
 }
