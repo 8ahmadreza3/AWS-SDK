b735ca2183405646ae738553f13f0ac80d29f979
8ahmadreza3 <8arhp3@gmail.com>
2024-08-08 12:11:12 +0330

    update packages

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-require('dotenv').config()
 const aws = require('./AWS')
-// const fs = require('fs')
+const fs = require('fs')
 
-// const awsKey = ''
-// const file = fs.readFileSync('')
+const file = fs.readFileSync('')
+const client = new aws({
+  endpoint: 'https://storage.iran.liara.space',
+  bucketName: 'kara-library',
+  accessKey: '59goe5pft0b35084',
+  secretKey: '82f9397e-9a12-463c-a860-1333253db49a',
+  bucketURL: 'https://kara-library.storage.iran.liara.space/',
+})
+
+console.log(client.upload(file, console.log))
 
 module.exports = aws
